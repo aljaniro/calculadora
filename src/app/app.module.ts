e95982bc9cb6444d054002179857e05ecf1a221d
Alvaro Nieto <anieto@fundacionlm.org>
2023-02-16 12:23:45 -0500

    first commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './Componentes/procesos/calculadora.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CalculadoraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
